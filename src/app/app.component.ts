7db68d3d67290f6a51b7803e869e090fb7eb4cbc
Luis Eduardo Ramirez Mora <luis.ramirezm@co.sonda.com>
2018-04-14 14:25:47 -0500

    Nuevos cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,18 +8,20 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
   title = 'Yes or No';
-  enabled = true;
   url = 'https://yesno.wtf/api';
   public imageSource;
+  public respuesta;
 
   constructor(private http: HttpClient){
     
   }
 
   mostrar(){
+    
     this.http.get(this.url).subscribe(data => {
       console.log(data);
       this.imageSource = data['image'];
+      this.respuesta = data['answer']=="yes"?"SI":"NO";
     });
   }
 }
